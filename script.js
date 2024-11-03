dffae7c0caa78861cad6a6d82c9b78620061c8e8
IiBodiI <100801126+IiBodiI@users.noreply.github.com>
2024-11-03 23:34:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Mesajınız başarıyla gönderildi! En kısa sürede sizinle iletişime geçeceğiz.");
+});
